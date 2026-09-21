825c8a0461b2dd8a72e848985bce65b570e4c1c6
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-22 01:50:32 +0900

    Mark PayPay as authoritative universe source

--- src/data/source_policy.py
+++ src/data/source_policy.py
@@ -9,12 +9,14 @@ class SourcePolicy:
     supports_news:bool=False
     supports_macro:bool=False
     requires_key:bool=False
+    authoritative:bool=False
 
 DEFAULT_SOURCES=(
-    SourcePolicy("jpx",1,supports_prices=True),
-    SourcePolicy("yfinance",2,supports_prices=True),
-    SourcePolicy("alpaca",3,supports_prices=True,requires_key=True),
-    SourcePolicy("massive",4,supports_prices=True,requires_key=True),
+    SourcePolicy("paypay_official",1,authoritative=True),
+    SourcePolicy("jpx",2,supports_prices=True),
+    SourcePolicy("yfinance",3,supports_prices=True),
     SourcePolicy("web_news",1,supports_news=True),
-    SourcePolicy("fred",1,supports_macro=True,requires_key=True),
+    SourcePolicy("alpaca",4,supports_prices=True,requires_key=True),
+    SourcePolicy("massive",5,supports_prices=True,requires_key=True),
+    SourcePolicy("fred",2,supports_macro=True,requires_key=False),
 )
