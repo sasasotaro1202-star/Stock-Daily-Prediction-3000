193880f69b6a3aba618b8bfb2e69513b092aa22f
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-24 00:26:38 +0900

    Align TOPIX symbol test with production proxy

--- tests/test_market_context_symbols.py
+++ tests/test_market_context_symbols.py
@@ -1,5 +1,5 @@
 from src.data.market_context import CONTEXT_SYMBOLS
 
 
-def test_topix_uses_current_yahoo_symbol():
-    assert CONTEXT_SYMBOLS["topix"] == "^TOPX"
+def test_topix_uses_topix_linked_etf_proxy():
+    assert CONTEXT_SYMBOLS["topix"] == "1306.T"
